f2fb8e2c2f0ffee58938a7eafa617b182af9b2f3
Levi McCallum <levi@levimccallum.com>
2025-02-09 16:12:43 -0800

    fix: unload error

--- custom_components/fellow_stagg/__init__.py
+++ custom_components/fellow_stagg/__init__.py
@@ -105,8 +105,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Fellow Stagg integration."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        coordinator = hass.data[DOMAIN].pop(entry.entry_id)
-        await coordinator.async_stop()
+        hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
 
 
